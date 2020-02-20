201bd42660be6db5a2b1f5949cf196b514dd6957
Alan Van Art <alanv73@gmail.com>
2020-02-20 14:05:25 -0500

    initial commit

--- omdbApp/ViewController.swift
+++ omdbApp/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblOutput: UILabel!
+    @IBOutlet weak var txtInput: UITextField!
+    
+    let api_key = "fa4b0499"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // http://www.omdbapi.com/?i=tt3896198&apikey=fa4b0499
+        
+        
     }
 
-
+    @IBAction func btnSearch(_ sender: Any) {
+        
+        
+        if let url = URL(string: "https://www.omdbapi.com/?t=" + txtInput.text!.replacingOccurrences(of: " ", with: "+") + "&apikey=" + api_key) {
+        
+            let task = URLSession.shared.dataTask(with: url) {
+                (data, response, error) in
+                
+                if error != nil {
+                    print(error!)
+                } else {
+                    if let urlContent = data {
+                        do {
+                        
+                            let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                            if var msgError = jsonResult["Error"] {
+                                if msgError == nil {
+                                    msgError = "no error"
+                                }
+                                
+                                print(msgError!)
+                                
+                                if (msgError as! String == "no error"){
+                                    print(jsonResult["Year"]!!)
+                                    print(jsonResult["Rated"]!!)
+                                    print(jsonResult["Director"]!!)
+                                    print(jsonResult["Actors"]!!)
+                                    print(jsonResult["Poster"]!!)
+                                }
+                            }
+                            
+                            
+                            
+                            
+                            
+                        
+                        } catch {
+                            print(error)
+                        }
+                
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
 }
 
