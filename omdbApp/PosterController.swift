c3f16a39090d1947f151181618f3d1dd3e73a175
Alan Van Art <avanart58@southhills.edu>
2020-02-24 20:05:02 -0500

    modal secondary vc button over image to dismiss vc format text beautify the interface

--- omdbApp/PosterController.swift
+++ omdbApp/PosterController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class PosterController: UIViewController {
     
-    @IBOutlet weak var lblPoster: UILabel!
+    
+    @IBOutlet weak var movieImage: UIImageView!
+    
+    @IBOutlet weak var btnPoster: UIButton!
+    
     public var posterURL: String = ""
     
 
@@ -20,7 +24,9 @@ class PosterController: UIViewController {
         // Do any additional setup after loading the view.
         
         if posterURL != "" {
-            lblPoster.text = posterURL
+            getPoster(posterURL)
+        } else {
+            btnPoster.setTitle("No Poster Found", for: UIControl.State.normal)
         }
     }
     
@@ -34,5 +40,30 @@ class PosterController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func btnPosterDismiss(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func getPoster (_ moviePoster: String) {
+        print(moviePoster)
+        
+        let url = URL(string:moviePoster)
 
+        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard let data = data, error == nil else { return }
+
+            DispatchQueue.main.async() {    // execute on main thread
+                let img = UIImage(data: data)
+                self.movieImage.image = img //UIImage(data: data)
+                self.movieImage.isHidden = false
+//                self.btnPoster.setImage(img, for: UIControl.State.normal)
+//                self.btnPoster.isHidden = false
+            }
+        }
+
+        task.resume()
+    }
+    
+    
 }
--- omdbApp/ViewController.swift
+++ omdbApp/ViewController.swift
@@ -8,34 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
     
     @IBOutlet weak var lblOutput: UILabel!
     @IBOutlet weak var txtInput: UITextField!
+    @IBOutlet weak var btnPoster: UIButton!
+    
     
     let api_key = "fa4b0499"
-    weak var omdb:OmdbDelegate?
-    
     var posterURL: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // http://www.omdbapi.com/?i=tt3896198&apikey=fa4b0499
         
+        btnPoster.isHidden = true
         
     }
     
     @IBAction func btnShowPoster(_ sender: Any) {
         print(posterURL)
-        let pvc: PosterController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PosterVC") as! PosterController
-        pvc.posterURL = posterURL
-        self.present(pvc, animated: true, completion: nil)
+        
+        performSegue(withIdentifier: "vcPoster", sender: sender)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "vcPoster" {
+            if let posterViewController = segue.destination as? PosterController {
+                posterViewController.posterURL = posterURL
+            }
+        }
+    }
+    
+    @IBAction func btnClear(_ sender: Any) {
+        txtInput.text = ""
+        txtInput.becomeFirstResponder()
     }
     
 
     @IBAction func btnSearch(_ sender: Any) {
         
         var msgError: String = ""
+        var msgOutput = ""
+        
         let apiUrl = "https://www.omdbapi.com/?t=" + txtInput.text!.replacingOccurrences(of: " ", with: "+") + "&apikey=" + api_key
         
         if let url = URL(string: apiUrl) {
@@ -58,23 +75,52 @@ class ViewController: UIViewController {
                             print(msgError)
                                 
                             if (msgError == "no error"){
-                                print(jsonResult["Year"]!!)
-                                print(jsonResult["Rated"]!!)
-                                print(jsonResult["Director"]!!)
-                                print(jsonResult["Actors"]!!)
-                                print(jsonResult["Poster"]!!)
+                                msgOutput = "\(jsonResult["Title"] as! NSString) (\(jsonResult["Year"] as! NSString))\n"
+                                msgOutput += "MPAA Rating: \(jsonResult["Rated"] as! NSString)\n"
+                                msgOutput += "Directed by: \(jsonResult["Director"] as! NSString)\n"
+                                var cast = "Cast: "
+                                
+                                let castArray = (jsonResult["Actors"] as! NSString).components(separatedBy: ", ")
+                                for actor in castArray {
+                                    cast += "\t" + actor + "\n\t\t"
+                                }
+                                
+                                msgOutput += cast
+                                
+                                print(jsonResult)
+
                                 self.posterURL = jsonResult["Poster"] as! String
+                                
+                                DispatchQueue.main.sync(execute: {
+                                    self.btnPoster.isHidden = false
+                                })
+                                
+                                
+                            } else {
+                                msgOutput = "Movie not found"
+                                self.posterURL = ""
+                                self.btnPoster.isHidden = true
                             }
+
+                            DispatchQueue.main.sync(execute: {
+                                self.lblOutput.frame = CGRect(x: 20, y: 245, width: 374, height: 408)
+                                self.lblOutput.text = msgOutput
+                                self.lblOutput.sizeToFit()
+                            })
                             
                         } catch {
                             print(error)
                         }
                 
                     }
+                    
                 }
+                
             }
             task.resume()
+            
         }
+        
     }
     
 }
